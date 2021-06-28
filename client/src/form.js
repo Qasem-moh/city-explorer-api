627e42f493918e5cfcd81ab62e3582dfca6eae97
qasem-moh <2171246@s.aou.edu.jo>
2021-06-28 21:04:02 +0300

    lab07 '

---
+++ client/src/form.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Form, Button, Image } from 'react-bootstrap';
+class GettingData extends React.Component{
+render(){
+    return(
+        <Form onSubmit={this.getData}>
+            <Form.Group className="mb-3" controlId="formBasicEmail" 	 >
+                <Form.Label>City Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter City Name" onChange={this.GetCityName} size={'sm'} />
+            </Form.Group>
+            <Button variant="primary" type="submit" >
+                Explore!
+            </Button>
+        </Form>
+    )
+}
+}
+
+
+export default GettingData
