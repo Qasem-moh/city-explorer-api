688591c41eac5bb09048d425b2576f6107e22e8e
qasem-moh <2171246@s.aou.edu.jo>
2021-06-29 23:20:19 +0300

    lab07

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,50 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const cors = require('cors');
 app.use(cors());
-require('dotenv').config();
-const port = 8888;
-const wheatherData = require('../data/weather.json')
+require("dotenv").config();
+const weather = require('../data/weather.json');
 
+const PORt = process.env.PORT
 app.get('/', (req, res) => {
-    res.send("Welcome")
-})
-app.get('/wheather', (req, res) => {
-    res.json({
-        lat: wheatherData.lat,
-        long: wheatherData.long,
-        searchQuery: ""
-    })
-
+    res.send('Hello World')
 })
 
-app.listen(port, () => console.log(`server is running on prot ${port}`))
+app.get('/weather', (req, res) => {
+    let lat = req.query.lat
+    let lon = req.query.lon
+    let searchQuery = req.query.searchQuery
+
+    console.log(lat);
+    console.log(lon);
+    console.log(searchQuery);
+
+
+
+
+
+    let findData = () => {
+
+        let city = weather.data.find((city, idx) => {
+            return weather.city_name.toLowerCase() === searchQuery.toLowerCase()
+        })
+        return weather.data.map(item => {
+            return new Forecast(item)
+        })
+    }
+    res.json(findData())
+})
+
+class Forecast {
+    constructor(weatherData) {
+        this.data = weatherData.valid_data;
+        this.description = weatherData.weather.description;
+    }
+}
+// app.get("*", (req, res) => {
+//     res.status(404).send("page not found");
+// });
+
+
+
+app.listen(process.env.PORT, () => console.log('Server is running'))
